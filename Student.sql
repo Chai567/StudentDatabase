64f3e2b5249b8bbf3d55e4e6f115209e0cac95a7
chaitanya.shah <chaitanya.shah@YI1011368LT>
2021-04-09 20:57:09 +0530

    Add project files.

---
+++ Student.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [StudentNo] INT          NOT NULL,
+    [Name]      VARCHAR (50) NOT NULL,
+    [Section]   INT          NOT NULL,
+    [Branch]    VARCHAR (50) NOT NULL,
+    [EmailId]   VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([StudentNo] ASC)
+);
+
